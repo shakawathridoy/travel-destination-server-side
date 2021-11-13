848538b2a3df3dbac001a9308b73cf1a141f4688
shakawathridoy <contact.freelancerhridoy@gmail.com>
2021-11-14 02:18:53 +0600

    4thh

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors')
 const ObjectId = require("mongodb").ObjectId;
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { restart } = require('nodemon');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,18 +19,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true });
-
-
-// async function run(){
-//     try{
-//         await client.connect();
-//         console.log('database connected')
-//     }
-//     finally{
-//         // await client.close()
-//     }
-// }
-// run().catch(console.dir);
 
 
 client.connect((err) => {
@@ -61,19 +50,52 @@ client.connect((err) => {
     app.post("/confirmBooking", async (req, res) => {
         const result = await bookingCollection.insertOne(req.body)
         res.send(result);
-    })
+    });
 
 
     // My Order
     app.get("/myBookings/:email", async (req, res) => {
         const result = await bookingCollection.find({ email: req.params.email}).toArray();
         res.send(result);
+    });
+
+
+    // Cancel Order 
+    app.delete("/cancelOrder/:id", async (req, res) => {
+        const result = await bookingCollection.deleteOne({_id: ObjectId(req.params.id)
+        });
+        res.send(result);
     })
+
+    // All Bookings
+
+    app.get("/allBooking", async (req, res) => {
+        const result = await bookingCollection.find({}).toArray()
+        res.send(result);
+    })
+
+    // update Status 
+    app.put("/updateStatus/:id", (req, res) => {
+        const id = req.params.id;
+        const updatedStatus = req.body.status;
+        const filter = {_id: ObjectId(id)};
+         bookingCollection.updateOne(filter, {
+            $set: {status: updatedStatus},
+        })
+        .then(result => {
+            res.send(result);
+        })
+        
+    })
+
 });
+
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || port);
+app.listen( port, () => {
+    console.log("Running Winter Server on port", port);
+});
